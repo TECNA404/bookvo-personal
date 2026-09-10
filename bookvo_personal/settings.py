7472a4fc9462273d41d506e97910392bc8bd9aeb
Anastasiia Chernova <tecna707@gmail.com>
2026-09-10 11:21:51 +0300

    Add JWT authentication

--- bookvo_personal/settings.py
+++ bookvo_personal/settings.py
@@ -59,9 +59,10 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticated"
+        "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
         "rest_framework.authentication.SessionAuthentication",
     ],
 }
